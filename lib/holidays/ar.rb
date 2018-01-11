9c15469aa384885a91cb56ead53a5f3dcdf054d8
Emanuel Alarcón <emanuel_cadems@hotmail.com>
2018-01-11 16:31:30 -0300

    Remove date decorator

--- lib/holidays/ar.rb
+++ lib/holidays/ar.rb
@@ -1,6 +1,5 @@
 require "holidays/ar/version"
 require "holidays/ar/loader"
-require "holidays/ar/date"
 
 module Holidays
   module Ar
--- lib/holidays/ar/date.rb
+++
@@ -1,6 +0,0 @@
-class Date
-  def holiday?
-    a = ENV['HOLIDAYS'].split(',')
-    a.include?(self.to_s)
-  end
-end
